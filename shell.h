4ccbcfae383a5be2ad4bbc78da5c8f4e7375a8e6
hollenaizer <avelounite@gmail.com>
2022-06-16 03:22:37 -0700

    updated shell.h

--- shell.h
+++ shell.h
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 #ifndef _SHELL_H_
 #define _SHELL_H_
-=======
-#ifndef _SHELL_H_
-#define _SHELL_H_
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
 
 #include <stdio.h>
 #include <unistd.h>
@@ -24,7 +19,6 @@
 /* Points to an array of pointers to strings called the "environment" */
 extern char **environ;
 
-<<<<<<< HEAD
 /**
  *
  * struct data - struct that contains all relevant data on runtime
@@ -70,45 +64,52 @@ typedef struct sep_list_s
 {
 char separator;
 struct sep_list_s *next;
-=======
 
 /**
  * struct data - struct that contains all relevant data on runtime
+ *
  * @av: argument vector
+ *
  * @input: command line written by the user
+ *
  * @args: tokens of the command line
+ *
  * @status: last status of the shell
+ *
  * @counter: lines counter
+ *
  * @_environ: environment variable
+ *
  * @pid: process ID of the shell
  */
 typedef struct data
 {
-	char **av;
-	char *input;
-	char **args;
-	int status;
-	int counter;
-	char **_environ;
-	char *pid;
+char **av;
+char *input;
+char **args;
+int status;
+int counter;
+char **_environ;
+char *pid;
 } data_shell;
 
 /**
  * struct sep_list_s - single linked list
+ *
  * @separator: ; | &
+ *
  * @next: next node
+ *
  * Description: single linked list to store separators
  */
 typedef struct sep_list_s
 {
-	char separator;
-	struct sep_list_s *next;
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
+char separator;
+struct sep_list_s *next;
 } sep_list;
 
 /**
  * struct line_list_s - single linked list
-<<<<<<< HEAD
  *
  * @line: command line
  *
@@ -122,21 +123,22 @@ typedef struct line_list_s
 {
 char *line;
 struct line_list_s *next;
-=======
+/*
  * @line: command line
+ *
  * @next: next node
+ *
  * Description: single linked list to store command lines
  */
+
 typedef struct line_list_s
 {
-	char *line;
-	struct line_list_s *next;
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
+char *line;
+struct line_list_s *next;
 } line_list;
 
 /**
  * struct r_var_list - single linked list
-<<<<<<< HEAD
  *
  * @len_var: length of the variable
  *
@@ -156,25 +158,27 @@ int len_var;
 char *val;
 int len_val;
 struct r_var_list *next;
-=======
+/*
  * @len_var: length of the variable
+ *
  * @val: value of the variable
+ *
  * @len_val: length of the value
+ *
  * @next: next node
+ *
  * Description: single linked list to store variables
  */
 typedef struct r_var_list
 {
-	int len_var;
-	char *val;
-	int len_val;
-	struct r_var_list *next;
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
+int len_var;
+char *val;
+int len_val;
+struct r_var_list *next;
 } r_var;
 
 /**
  * struct builtin_s - Builtin struct for command args.
-<<<<<<< HEAD
  *
  * @name: The name of the command builtin i.e cd, exit, env
  *
@@ -186,15 +190,15 @@ typedef struct builtin_s
 {
 char *name;
 int (*f)(data_shell *datash);
-=======
+/*
  * @name: The name of the command builtin i.e cd, exit, env
+ *
  * @f: data type pointer function.
  */
 typedef struct builtin_s
 {
-	char *name;
-	int (*f)(data_shell *datash);
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
+char *name;
+int (*f)(data_shell *datash);
 } builtin_t;
 
 /* aux_lists.c */
@@ -313,19 +317,12 @@ char *error_syntax(char **args);
 char *error_permission(char **args);
 char *error_path_126(data_shell *datash);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
 /* get_error.c */
 int get_error(data_shell *datash, int eval);
 
 /* get_sigint.c */
 void get_sigint(int sig);
-<<<<<<< HEAD
-=======
 
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
 /* aux_help.c */
 void aux_help_env(void);
 void aux_help_setenv(void);
@@ -340,8 +337,5 @@ void aux_help_cd(void);
 
 /* get_help.c */
 int get_help(data_shell *datash);
-<<<<<<< HEAD
-=======
 
->>>>>>> 422233c3bc5cedbbf5cc6b86650d93d4ce418874
 #endif
