123cebf7ac8f52c31ee8cd4c1f8150e0254b0c73
hollenaizer <avelounite@gmail.com>
2022-06-16 03:29:53 -0700

    update get_error.c

--- get_error.c
+++ get_error.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "shell.h"
 
 /**
  * get_error - calls the error according the builtin, syntax or permission
@@ -8,33 +8,31 @@
  */
 int get_error(data_shell *datash, int eval)
 {
-	char *error;
+char *error;
 
-	switch (eval)
-	{
-	case -1:
-		error = error_env(datash);
-		break;
-	case 126:
-		error = error_path_126(datash);
-		break;
-	case 127:
-		error = error_not_found(datash);
-		break;
-	case 2:
-		if (_strcmp("exit", datash->args[0]) == 0)
-			error = error_exit_shell(datash);
-		else if (_strcmp("cd", datash->args[0]) == 0)
-			error = error_get_cd(datash);
-		break;
-	}
-
-	if (error)
-	{
-		write(STDERR_FILENO, error, _strlen(error));
-		free(error);
-	}
-
-	datash->status = eval;
-	return (eval);
+switch (eval)
+{
+case -1:
+error = error_env(datash);
+break;
+case 126:
+error = error_path_126(datash);
+break;
+case 127:
+error = error_not_found(datash);
+break;
+case 2:
+if (_strcmp("exit", datash->args[0]) == 0)
+error = error_exit_shell(datash);
+else if (_strcmp("cd", datash->args[0]) == 0)
+error = error_get_cd(datash);
+break;
+}
+if (error)
+{
+write(STDERR_FILENO, error, _strlen(error));
+free(error);
+}
+datash->status = eval;
+return (eval);
 }
