ef543c7e0090d8b5682792dfaff26161799dc443
hollenaizer <avelounite@gmail.com>
2022-06-17 02:02:50 -0700

    updating env1.c

--- env1.c
+++ env1.c
@@ -2,8 +2,11 @@
 
 /**
  * cmp_env_name - compares env variables names
+ *
  * with the name passed.
+ *
  * @nenv: name of the environment variable
+ *
  * @name: name passed
  *
  * Return: 0 if are not equal. Another value if they are.
